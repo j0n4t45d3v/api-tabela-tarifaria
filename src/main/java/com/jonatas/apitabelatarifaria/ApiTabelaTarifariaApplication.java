14d087e05b08aaeb9d277e2f245b0f21480eeefb
Jonatas <delimajonatas875@gmail.com>
2026-08-04 10:29:43 -0300

    commit inicial

---
+++ src/main/java/com/jonatas/apitabelatarifaria/ApiTabelaTarifariaApplication.java
@@ -0,0 +1,13 @@
+package com.jonatas.apitabelatarifaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTabelaTarifariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTabelaTarifariaApplication.class, args);
+    }
+
+}
