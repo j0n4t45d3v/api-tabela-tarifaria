d24a5ef954edcc78b5fa6e9cd851936989257c97
Jonatas <jonatas.melo@3lminformatica.com.br>
2026-08-05 13:50:50 -0300

    adiciona validações para não permitir valores negativos ou valores maiores do que 99999

--- src/main/java/com/jonatas/apitabelatarifaria/dto/FaixaConsumoRequest.java
+++ src/main/java/com/jonatas/apitabelatarifaria/dto/FaixaConsumoRequest.java
@@ -2,14 +2,18 @@ package com.jonatas.apitabelatarifaria.dto;
 
 import java.math.BigDecimal;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record FaixaConsumoRequest(
 
     @NotNull(message = "Informe 'faixas[].de' da faixa de consumo.")
+    @Positive(message = "Informe somente valores positivos")
+    @Max(value = 99999, message = "Valor máximo permitido é 99999")
     Integer de,
 
     @NotNull(message = "Informe 'faixas[].ate' da faixa de consumo.")
+    @Positive(message = "Informe somente valores positivos")
+    @Max(value = 99999, message = "Valor máximo permitido é 99999")
     Integer ate,
 
     @NotNull(message = "Informe 'faixas[].valorUnitario' da faixa de consumo.")
