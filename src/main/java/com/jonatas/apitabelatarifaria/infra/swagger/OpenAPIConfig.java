85e50d1d2f58402ce6d925c724112d98018a6b4e
Jonatas <jonatas.melo@3lminformatica.com.br>
2026-08-06 10:08:52 -0300

    torna opcional a injeção do BuildProperties na configuração do OpenAPI

--- src/main/java/com/jonatas/apitabelatarifaria/infra/swagger/OpenAPIConfig.java
+++ src/main/java/com/jonatas/apitabelatarifaria/infra/swagger/OpenAPIConfig.java
@@ -1,5 +1,6 @@
 package com.jonatas.apitabelatarifaria.infra.swagger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +8,22 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import java.util.Optional;
+
 @Configuration
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI openApi(BuildProperties properties) {
+    public OpenAPI openApi(@Autowired(required = false) BuildProperties properties) {
         return new OpenAPI().info(info(properties));
     }
 
     private Info info(BuildProperties properties) {
+        var version = Optional.ofNullable(properties)
+                .map(BuildProperties::getVersion)
+                .orElse("dev");
         return new Info()
                 .title("API de Tabela Tarifária de Água")
-                .version(properties.getVersion());
+                .version(version);
     }
 }
